8d0d04a5f874ea1eb348d72c709729109b5040ac
maeosha <mochkauha123@gmail.com>
2024-09-03 14:59:52 +0400

    Add methods for adding, removing edges and checking edge existence

--- main.cpp
+++ main.cpp
@@ -55,4 +55,62 @@ public:
         graphList.erase(vertex);
         return true;
     }
+
+    void add_edge(const Vertex& from, const Vertex& to, const Distance& dist) {
+        add_vertex(from);
+        add_vertex(to);
+        graphList[from].push_back(Edge(from, to, dist));
+    }
+
+    bool remove_edge(const Vertex& from, const Vertex& to) {
+        if (graphList.find(from) != graphList.end()) {
+            auto& edges = graphList[from];
+            auto it = edges.begin();
+            while (it != edges.end()) {
+                if (it->from == from && it->to == to) {
+                    it = edges.erase(it);
+                    return true;
+                } else {
+                    ++it;
+                }
+            }
+        }
+
+        if (graphList.find(to) != graphList.end()) {
+            auto& edges = graphList[to];
+            auto it = edges.begin();
+            while (it != edges.end()) {
+                if (it->from == from && it->to == to) {
+                    it = edges.erase(it);
+                    return true;
+                } else {
+                    ++it;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool remove_edge(const Edge& del_edge) {
+        return remove_edge(del_edge.from, del_edge.to);
+    }
+
+    bool has_edge(const Vertex& from, const Vertex& to) const {
+        if (!has_vertex(from) || !has_vertex(to)) {
+            return false;
+        }
+
+        for (const auto& edge : graphList.at(from)) {
+            if (edge.from == from && edge.to == to) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool has_edge(const Edge& edge) const {
+        return has_edge(edge.from, edge.to);
+    }
 };
