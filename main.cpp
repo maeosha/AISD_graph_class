697fa88853dd191c806a2be343aa83866f1aef25
maeosha <mochkauha123@gmail.com>
2024-09-03 15:00:33 +0400

    Add methods to get average distance, retrieve edges, and compute shortest path

--- main.cpp
+++ main.cpp
@@ -113,4 +113,128 @@ public:
     bool has_edge(const Edge& edge) const {
         return has_edge(edge.from, edge.to);
     }
+
+    float get_average_dist(const Vertex& vertex) const {
+        if (!has_vertex(vertex)) {
+            return 0;
+        }
+        Distance sum_dist = 0;
+        size_t count_of_edges = graphList.at(vertex).size();
+
+        if (count_of_edges == 0) {
+            return 0;
+        }
+
+        for (const auto& edge : graphList.at(vertex)) {
+            sum_dist += edge.dist;
+        }
+        return static_cast<float>(sum_dist) / count_of_edges;
+    }
+
+    std::vector<Vertex> vertices() const {
+        std::vector<Vertex> vertices;
+        for (const auto& [vert, edges] : graphList) {
+            vertices.push_back(vert);
+        }
+        return vertices;
+    }
+
+    std::vector<Edge> edges(const Vertex& vertex) const {
+        if (!has_vertex(vertex)) {
+            return {};
+        }
+
+        std::vector<Edge> edges_of_vert;
+        for (const auto& [vert, edges] : graphList) {
+            for (const auto& edge : edges) {
+                if (edge.from == vertex || edge.to == vertex) {
+                    edges_of_vert.push_back(edge);
+                }
+            }
+        }
+
+        return edges_of_vert;
+    }
+
+    std::vector<Edge> edges() const {
+        std::vector<Edge> edges_list;
+        for (const auto& [u, edges] : graphList) {
+            for (const auto& edge : edges) {
+                edges_list.push_back(edge);
+            }
+        }
+        return edges_list;
+    }
+
+    size_t order() const {
+        return graphList.size();
+    }
+
+    size_t degree(const Vertex& vertex) const {
+        if (!has_vertex(vertex)) {
+            return 0;
+        }
+        return graphList.at(vertex).size();
+    }
+
+    std::vector<Edge> shortest_path(const Vertex& from, const Vertex& to) const {
+        std::map<Vertex, Vertex> pred;
+        std::map<Vertex, Distance> dist;
+        std::map<Vertex, bool> dist_updated;
+
+        size_t updated_count = 0;
+
+        if (!has_vertex(from) || !has_vertex(to)) {
+            return {};
+        }
+
+        for (const auto& [vert, edge] : graphList) {
+            dist[vert] = INFINITY;
+            dist_updated[vert] = false;
+        }
+
+        dist[from] = 0;
+        updated_count++;
+        dist_updated[from] = true;
+
+        for (size_t i = 0; i < order(); ++i) {
+            for (const auto& [u, edges] : graphList) {
+                if (dist_updated[u]) {
+                    for (const auto& edge : edges) {
+                        if ((edge.dist + dist[u]) < dist[edge.to]) {
+                            dist[edge.to] = edge.dist + dist[u];
+                            pred[edge.to] = u;
+                            if (!dist_updated[edge.to]) {
+                                updated_count++;
+                                dist_updated[edge.to] = true;
+                            }
+                        }
+                    }
+                    dist_updated[u] = false;
+                    updated_count--;
+                }
+                if (updated_count == 0) {
+                    break;
+                }
+            }
+        }
+
+        if (updated_count != 0) {
+            throw std::runtime_error("Graph contains a negative-weight cycle");
+        }
+
+        std::vector<Edge> path;
+        Vertex current = to;
+
+        while (current != from) {
+            if (pred.find(current) == pred.end()) {
+                return {};
+            }
+            Vertex predecessor = pred[current];
+            Distance weight = dist[current] - dist[predecessor];
+            path.emplace(path.begin(), predecessor, current, weight);
+            current = predecessor;
+        }
+        return path;
+    }
 };
